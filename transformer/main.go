68c1acd3b498c01b6af32e1173f443dc3f0dcd82
David Kalmin <dkalmin@github.com>
2025-12-10 14:18:54 -0800

    Update main.go

--- transformer/main.go
+++ transformer/main.go
@@ -1,27 +1,20 @@
 package main
 
 import (
-	"log"
-	"os"
+	transformer "gh/transform/transformer"
+	"fmt"
 )
 
 func main() {
-	// Create or open a log file
-	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatal("Failed to open log file:", err)
-	}
-	defer logFile.Close()
-
-	// Set up logger to write to both file and console
-	log.SetOutput(logFile)
+	println("Start of transform")
 	
-	// Log some messages
-	log.Println("Application started")
-	log.Println("This is an info message")
-	log.Println("Processing data...")
-	log.Println("Application completed successfully")
+	transformResult := transformer.Transform()
 
-	// Also print to console
-	println("Logs have been written to app.log")
+	if transformResult != true {
+			fmt.Printf("Expected true, got '%v'", transformResult)
+		} else {
+			println("We transformed!")
+		}
+
+	println("End of transform")
 }
